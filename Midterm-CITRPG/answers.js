b47007cdbb9eb8a741b56c7f58392494b084dfe3
lacip-pixel <lacip@umich.edu>
2024-03-19 19:00:08 -0400

    level 1

--- Midterm-CITRPG/answers.js
+++ Midterm-CITRPG/answers.js
@@ -1,44 +1,65 @@
-const button = document.querySelector("input");
+var lives = 3;
 
+// Function to initialize the count of correct answers from local storage
+function initCorrectCount() {
+    // Check if the count is already stored in local storage
+    if (!localStorage.getItem('correctCount')) {
+        // If not, initialize it to 0
+        localStorage.setItem('correctCount', 0);
+    }
+}
 
-function highlightAnswerWithClass(question, answer, className) {
-    var answers = document.forms.form;
-    for (var index = question*4; index < question*4 + 4; index++) {
-      if (answers[index].value === answer) {
-        answers[index].classList.add(className);
-      }
-    }}
-    function score() {
-        //Referencing the value of the questions
-        var q1 = document.forms.form.q1.value;
-     
-      
-        // disable form
-        var elements = document.getElementById("questions").elements;
-        for (var i = 0, len = elements.length; i < len; ++i) {
-          elements[i].disabled = true;
+// Function to increment the count of correct answers and update local storage
+function incrementCorrectCount() {
+    localStorage.setItem('correctCount', parseInt(localStorage.getItem('correctCount')) + 1);
+}
+
+// Function to check the answer
+function score(answer) {
+    if (answer === 'm#P52s@ap$V') {
+        alert('Correct!');
+        // Increment the count of correct answers
+        incrementCorrectCount();
+        // Redirect to the next page
+        window.location.href = '/html/level1.html'; // Replace 'next_page.html' with the URL of the next page
+    } else {
+        alert('Wrong answer. You have ' + --lives + ' lives remaining.');
+        if (lives === 0) {
+            // If no lives remaining, redirect to game-over page
+            window.location.href = '/html/gameover.html'; 
         }
-      
-        //Array for the questions
-        var questions = [q1];
-      
-        //Answers for each question
-        var answers = ["m#P52s@ap$V"];
-      
-        //variable to keep track of the points
-        var points = 0;
-        var total = 1;
-        //max score
-      
-        //Making use of a for loop to iterate over the questions and answers arrays
-        for (var i = 0; i < total; i++) {
-          
-          if (questions[i] == answers[i]) {
-            points = points + 2; //Increment the score by 2 for every correct answer given
-            highlightAnswerWithClass(i, questions[i], "correct");
-          } else {
-            points = points - 1;
-            highlightAnswerWithClass(i, questions[i], "incorrect");
-            highlightAnswerWithClass(i, answers[i], "correct");
-          }
-        }}
+    }
+}
+function score2(answer) {
+  if (answer === 'HyperText Transfer Protocol Secure') {
+      alert('Correct!');
+      // Increment the count of correct answers
+      incrementCorrectCount();
+      // Redirect to the next page
+      window.location.href = '/html/level1.html'; // Replace 'next_page.html' with the URL of the next page
+  } else {
+      alert('Wrong answer. You have ' + --lives + ' lives remaining.');
+      if (lives === 0) {
+          // If no lives remaining, redirect to game-over page
+          window.location.href = '/html/gameover.html'; 
+      }
+  }
+}
+function score3(answer) {
+  if (answer === 'Spoofed emails') {
+      alert('Correct!');
+      // Increment the count of correct answers
+      incrementCorrectCount();
+      // Redirect to the next page
+      window.location.href = '/html/level1.html'; // Replace 'next_page.html' with the URL of the next page
+  } else {
+      alert('Wrong answer. You have ' + --lives + ' lives remaining.');
+      if (lives === 0) {
+          // If no lives remaining, redirect to game-over page
+          window.location.href = '/html/gameover.html'; 
+      }
+  }
+}
+
+// Initialize the count of correct answers when the page loads
+initCorrectCount();
